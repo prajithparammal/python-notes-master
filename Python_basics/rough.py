8120ef8151718e2906cf27f0932666608dabeb42
Prajith-office-laptop <prajithparammal@gmail.com>
2020-02-25 23:49:11 +0800

    update rough.py

--- Python_basics/rough.py
+++ Python_basics/rough.py
@@ -1,7 +1,35 @@
-def ran_check (num, low, high):
-	if num in range (low,high):
-		print ("%s is in the range" %str(num))
-	else:
-		print ("The number is outside the range")
+class dog():
 
-ran_check (5, 3, 8)
+    species = 'mammal'
+    def __init__(self,breed):
+        self.breed = breed
+
+sam = dog(breed='lab')
+frank = dog(breed='huskie')
+
+print(sam.breed)
+print(frank.breed)
+print(sam.species)
+
+
+class circle():
+    pi = 3.14
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    def area(self):
+        return self.radius * self.radius * circle.pi
+
+    def setRadius(self, radius):
+        self.radius = radius
+
+    def getradius(self):
+        return self.radius
+
+c = circle()
+c.setRadius(2)
+print('radius is :', c.getradius())
+print('area is :', c.area())
+
+
+
