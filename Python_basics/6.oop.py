d0b67fa2d25b074df7b06d401e16ca18106eb97b
Prajith-office-laptop <prajithparammal@gmail.com>
2020-03-03 11:08:36 +0800

    update rough.py

--- Python_basics/6.oop.py
+++ Python_basics/6.oop.py
@@ -81,3 +81,82 @@ my_dog_3.barknum(3)
 
 ######################################################
 
+class Circle():
+    ## Class Object Attribute
+    pi = 3.14
+
+    def __init__(self,radius=1):
+        self.radius = radius
+        self.area = radius * radius * self.pi ## or self.area = radius * radius * Circle.pi
+
+    # Method
+    def get_circumfrence(self):
+        return self.radius *self.pi * 2
+
+my_circle = Circle(30)
+print ("My circle circumfrence is", my_circle.get_circumfrence())
+print ("My circle area is", my_circle.area)
+######################################################
+
+## Inheritance are way to form new class using class which are already defined.
+
+class Animal():
+    def __init__(self):  # init method will automatically executed , when you call animal class
+        print("Animal created")
+    def who_ami_i(self):
+        print ("I am an animal")
+    def eat(self):
+        print ("I am eating")
+
+class Dog(Animal):  ## Derived class, becoz it is deriving some features of base class Animal
+    def __init__(self):
+        Animal.__init__(self) ##create an instance of animal class
+        print("Dog Created")
+    def who_ami_i(self):
+        print ("I am an dog")
+
+
+myanimal = Animal()     ## myanimal is an instance of class
+print (myanimal.eat())
+print (myanimal.who_ami_i())
+
+my_dog = Dog()
+print (my_dog)
+print (my_dog.eat())
+print (my_dog.who_ami_i())
+
+######################################################
+## Polymorphism is different object class can share the same method name.
+
+class Dog():
+    def __init__(self,name):
+        self.name = name
+    def speak(self):
+        return self.name + "says woof!!"
+
+
+class Cat():
+    def __init__(self,name):
+        self.name = name
+    def speak(self):
+        return self.name + "says meow!!"
+
+niko = Dog ("niko ")
+felix = Cat ("felix ")
+
+print (niko.speak())
+print (felix.speak())
+
+for pet in [niko,felix]:
+    print (type(pet))
+    print(type(pet.speak()))
+
+######################################################
+
+def pet_speak(pet):
+    print (pet.speak())
+
+pet_speak(niko)
+pet_speak(felix)
+######################################################
+
